916f7fe237b87896032127391851bd1f53fce719
dvdtoth <dvdtoth@users.noreply.github.com>
2019-01-26 22:30:14 +0000

    boto3, yaml, formatting

--- src/oanda-poller.py
+++ src/oanda-poller.py
@@ -4,10 +4,10 @@ import json
 import dateutil.parser
 import time
 import sys
-#sys.path.append('./src/')
-#from FWLiveParams import FWLiveParams
 from logger import logger
 from kafka import KafkaProducer
+import yaml
+import boto3
 
 # Parse config
 with open(sys.argv[1], 'r') as config_file:
@@ -25,18 +25,18 @@ def getOandaCredentials():
     ssm = boto3.client('ssm', region_name='eu-west-1')
 
     return {
-            'accountid' : FWLiveParams.getSSMParam(ssm,'/prod/exchange/oanda/accountid'),
-            'apikey' : FWLiveParams.getSSMParam(ssm,'/prod/exchange/oanda/apikey')
+            'accountid' : getSSMParam(ssm, '/prod/exchange/oanda/accountid'),
+            'apikey' : getSSMParam(ssm, '/prod/exchange/oanda/apikey')
             }
 
-async def pollForex(symbols, authkey,accountid):
+async def pollForex(symbols, authkey, accountid):
     i = 0
     while True:
         symbol = symbols[i % len(symbols)]
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(
-                        url="https://api-fxpractice.oanda.com/v3/accounts/"+accountid+"/pricing",
+                        url="https://api-fxpractice.oanda.com/v3/accounts/" + accountid + "/pricing",
                         headers={'Authorization': ('Bearer ' + authkey)},
                         params='instruments=' + symbol,
                         proxy=config['proxy']['address']) as resp:
@@ -48,7 +48,7 @@ async def pollForex(symbols, authkey,accountid):
         await asyncio.sleep(delay)
 
 async def forexPoller(symbols, authkey, accountid, orderbookAnalyser):
-    async for ticker in pollForex(symbols=symbols, authkey=authkey,accountid=accountid):
+    async for ticker in pollForex(symbols=symbols, authkey=authkey, ccountid=accountid):
         try:
             symbolBase = ticker['prices'][0]['instrument'].split("_")[0]
             symbolQuote = ticker['prices'][0]['instrument'].split("_")[1]
@@ -58,10 +58,10 @@ async def forexPoller(symbols, authkey, accountid, orderbookAnalyser):
             payload['exchange'] = "oanda"
             payload['symbol'] = symbolBase + "/" + symbolQuote
             payload['data'] = {}
-            payload['data']['asks'] = [[float(asks[0]['price']),asks[0]['liquidity']]]
-            payload['data']['bids'] = [[float(bids[0]['price']),bids[0]['liquidity']]]
+            payload['data']['asks'] = [[float(asks[0]['price']), asks[0]['liquidity']]]
+            payload['data']['bids'] = [[float(bids[0]['price']), bids[0]['liquidity']]]
             payload['timestamp'] = time.mktime(dateutil.parser.parse(ticker['time']).timetuple())
-            logger.info("Received " + symbolBase+"/"+ symbolQuote + " prices from Oanda")
+            logger.info("Received " + symbolBase+"/" + symbolQuote + " prices from Oanda")
             
             p = exchange.json(payload)
             kafka_producer.send(config['kafka']['topic'], p)
@@ -69,7 +69,7 @@ async def forexPoller(symbols, authkey, accountid, orderbookAnalyser):
         except Exception as error:
             logger.error("Error interpreting Oanda ticker: " + type(error).__name__ + " " + str(error.args))
 
-oandaCredentials=getOandaCredentials()
+oandaCredentials = getOandaCredentials()
 asyncio.ensure_future(
     forexPoller(
         symbols=config.exchange.symbols,
