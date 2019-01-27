0e92acee3dd61f4e022694b3417b7abd0f19333d
dvdtoth <dvdtoth@users.noreply.github.com>
2019-01-27 13:07:39 +0000

    Added sfox ws listener

---
+++ src/sfox-listener.py
@@ -0,0 +1,53 @@
+import asyncio
+import websockets
+import json
+from logger import logger
+import sys
+from kafka import KafkaProducer
+import yaml
+import time
+
+# Parse config
+with open(sys.argv[1], 'r') as config_file:
+    config = yaml.load(config_file)
+
+kafka_producer = KafkaProducer(bootstrap_servers=config['kafka']['address'], value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+async def sfoxWebSocket(symbols):
+    async with websockets.connect('wss://ws.sfox.com/ws') as ws:
+        subscribeMsg = {
+            "type": "subscribe",
+            "feeds": list(map(lambda symbol:'orderbook.sfox.' + symbol, symbols))
+        }
+        await ws.send(json.dumps(subscribeMsg))
+        resp = await ws.recv()
+
+        try:
+            msg = json.loads(resp)
+            if msg["type"] != "success":
+                logger.error("Subscribtion to SFOX unsuccessful, response type=" + msg["type"])
+                return
+        except Exception as error:
+            logger.error("Failed to subscribe to SFOX web socket: " + type(error).__name__ + " " + str(error.args))
+
+        async for message in ws:
+            try:
+                msg = json.loads(message)
+                symbol = msg['recipient'].split('.')[2].upper()
+                symbol = symbol[0:3]+"/"+symbol[3:]
+                payload = {}
+                payload['exchange'] = "sfox"
+                payload['symbol'] = symbol
+                payload['data'] = {}
+                payload['data']['asks'] = list(map(lambda entry:entry[0:2], msg['payload']['asks']))
+                payload['data']['bids'] = list(map(lambda entry:entry[0:2], msg['payload']['bids']))
+                payload['timestamp'] = msg['timestamp']/1e6
+                logger.info("Received " + symbol + " prices from SFOX")
+                
+                p = json.dumps(payload, separators=(',', ':'))
+                kafka_producer.send(config['kafka']['topic'], p)
+
+            except Exception as error:
+                logger.warn("Error while parsing SFOX websocket data: " + type(error).__name__ + " " + str(error.args))
+
+asyncio.get_event_loop().run_until_complete(sfoxWebSocket(symbols=config['exchange']['symbols'],))
