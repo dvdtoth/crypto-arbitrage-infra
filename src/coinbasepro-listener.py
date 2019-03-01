dd58b52b40bb0f73f218772add152dc2b2fdb0ee
Gabor Barany <gabor@cleenup.com>
2019-03-01 22:05:32 +0000

    multiprocessing

--- src/coinbasepro-listener.py
+++ src/coinbasepro-listener.py
@@ -7,6 +7,7 @@ import json
 from kafka import KafkaProducer
 import dateutil.parser
 import time
+from multiprocessing import Process, Event
 from CWMetrics import CWMetrics
 
 # Parse config
@@ -24,44 +25,47 @@ class CryptoArbOrderBook(cbpro.OrderBook):
         self.bidsConsolidatedOld = []
         self.timeLastRun = time.time()
         self.timeLimiterSeconds = timeLimiterSeconds
-        return super().__init__(product_id=product_id, log_to=log_to)
+        super().__init__(product_id=product_id, log_to=log_to)
 
     def on_message(self, message):
-        if (time.time() - self.timeLastRun)<self.timeLimiterSeconds:
-            return super().on_message(message)
+        super().on_message(message)
+                
+        if (time.time() - self.timeLastRun) > self.timeLimiterSeconds:
+            try:
+                book = self.get_current_book()
+                if len(book["asks"])>0 and len(book["bids"])>0:
+                    asksConsolidated = self.getConsolidatedOrderbook(book["asks"], reverse=False)
+                    bidsConsolidated = self.getConsolidatedOrderbook(book["bids"], reverse=True)
+                    print("asks:"+str(asksConsolidated)+", bids:"+str(bidsConsolidated))
 
-        book=self.get_current_book()
-        if len(book["asks"])>0 and len(book["bids"])>0:
-            asksConsolidated = self.getConsolidatedOrderbook(book["asks"], reverse=False)
-            bidsConsolidated = self.getConsolidatedOrderbook(book["bids"], reverse=True)
-            logger.info("asks:"+str(asksConsolidated)+", bids:"+str(bidsConsolidated))
+                    if self.asksConsolidatedOld != asksConsolidated or self.bidsConsolidatedOld != bidsConsolidated:
+                        payload = {}
+                        payload['exchange'] = "coinbasepro"
+                        payload['symbol'] = message['product_id'].replace('-','/')
+                        payload['data'] = {}
+                        payload['data']['asks'] = asksConsolidated
+                        payload['data']['bids'] = bidsConsolidated
+                        dt = dateutil.parser.parse(message['time'])
+                        payload['timestamp'] = int(time.mktime(dt.timetuple()) * 1000 + dt.microsecond / 1000)
 
-            if self.asksConsolidatedOld != asksConsolidated or self.bidsConsolidatedOld != bidsConsolidated:
-                payload = {}
-                payload['exchange'] = "coinbasepro"
-                payload['symbol'] = message['product_id'].replace('-','/')
-                payload['data'] = {}
-                payload['data']['asks'] = asksConsolidated
-                payload['data']['bids'] = bidsConsolidated
-                dt = dateutil.parser.parse(message['time'])
-                payload['timestamp'] = int(time.mktime(dt.timetuple()) * 1000 + dt.microsecond / 1000)
+                        p = json.dumps(payload, separators=(',', ':'))
+                        kafka_producer.send(config['kafka']['topic'], p)
 
-                p = json.dumps(payload, separators=(',', ':'))
-                kafka_producer.send(config['kafka']['topic'], p)
+                        logger.info("Received " + payload['symbol'] + " prices from coinbasepro")
+                        metrics.put(payload['timestamp'])
 
-                logger.info("Received " + payload['symbol'] + " prices from coinbasepro")
-                metrics.put(payload['timestamp'])
+                        self.asksConsolidatedOld = asksConsolidated
+                        self.bidsConsolidatedOld = bidsConsolidated
 
-                self.asksConsolidatedOld = asksConsolidated
-                self.bidsConsolidatedOld = bidsConsolidated
+                    if book["asks"][0][0] <= book["bids"][-1][0]:
+                        logger.error("Bid higher than ask")
+                        metrics.putError()
+            except Exception as err:
+                logger.error("Error during message processing:" + str(err))
+            finally:
+                self.timeLastRun = time.time()
 
-            if book["asks"][0][0]<=book["bids"][-1][0]:
-                logger.error("Bid higher than ask")
-                metrics.putError()
-        self.timeLastRun = time.time()
-        return super().on_message(message)
-
-    def getConsolidatedOrderbook(self,entries,reverse=False):
+    def getConsolidatedOrderbook(self, entries, reverse=False):
         orderbook = []
         
         size = Decimal(0)
@@ -86,19 +90,28 @@ class CryptoArbOrderBook(cbpro.OrderBook):
         
         return orderbook
         
-    
+def CryptoArbOrderBookProcess(pair,stopProcessesEvent):
+    orderbook=CryptoArbOrderBook(product_id=pair.replace('/', '-'))
+    orderbook.start()
+    stopProcessesEvent.wait()
+    orderbook.close()
+
 pairs = config['exchange']['symbols']
 
-# start orderbooks
-ordersbooks = []
-for pair in pairs:
-    orderbook = CryptoArbOrderBook(product_id=pair.replace('/','-'))
-    ordersbooks.append(orderbook)
-    orderbook.start()
+stopProcessesEvent = Event()
+processes = [Process(target=CryptoArbOrderBookProcess, args=(pair,stopProcessesEvent)) for pair in pairs]
 
-while True:
-    time.sleep(1)
+# start order books
+for process in processes:
+    process.daemon = True
+    process.start()
 
-# stop orderbooks
-for orderbook in ordersbooks:
-    orderbook.close()
+input("Press Enter to stop")
+
+stopProcessesEvent.set()
+for process in processes:
+    process.join()
+
+logger.info("coinbaseproWebsocket exited normally. Bye.")
+
+
