9a9d68ec739c89c83d04cc51bcd7718eb15dc027
Gabor Barany <gabor@cleenup.com>
2019-03-17 13:45:09 +0000

    change orderbook depth to 30

--- src/binance-listener.py
+++ src/binance-listener.py
@@ -36,24 +36,28 @@ def getCredentials():
 
 # configurable parameters
 orderbookDepth = 20
-restartPeriodSeconds = 4*3600
+restartPeriodSeconds = 3*3600
 
 credentials = getCredentials()
 client = Client(api_key=credentials['api_key'],
                 api_secret=credentials['api_secret'])
 
+
 def process_message(msg):
-    payload = dict()
-    payload['exchange'] = "binance"
-    payload['symbol'] = pairBinanceNameMapping[msg['stream']]
-    payload['data'] = {}
-    payload['data']['asks'] = list(map(lambda entry:[float(entry[0]), float(entry[1])], msg['data']['asks']))
-    payload['data']['bids'] = list(map(lambda entry:[float(entry[0]), float(entry[1])], msg['data']['bids']))
-    payload['timestamp'] = time.time()*1000
+    try:
+        payload = dict()
+        payload['exchange'] = "binance"
+        payload['symbol'] = pairBinanceNameMapping[msg['stream']]
+        payload['data'] = {}
+        payload['data']['asks'] = list(map(lambda entry:[float(entry[0]), float(entry[1])], msg['data']['asks']))
+        payload['data']['bids'] = list(map(lambda entry:[float(entry[0]), float(entry[1])], msg['data']['bids']))
+        payload['timestamp'] = time.time()*1000
 
-    p = json.dumps(payload, separators=(',', ':'))
-    kafka_producer.send(config['kafka']['topic'], p)
-
+        p = json.dumps(payload, separators=(',', ':'))
+        kafka_producer.send(config['kafka']['topic'], p)
+    except Exception as error:
+        logger.error("Error in Binance web socket connection: " + type(error).__name__ + " " + str(error.args))
+        metrics.putError()
 
 pairList = []
 pairBinanceNameMapping = dict()
@@ -69,8 +73,12 @@ for pair in config['exchange']['symbols']:
     pairBinanceNameMapping[pairElement] = pair
 
 while True:
-    bm = BinanceSocketManager(client)
-    bm.start_multiplex_socket(pairList, process_message)
-    bm.start()
-    time.sleep(restartPeriodSeconds)
-    bm.close()
+    try:
+        bm = BinanceSocketManager(client)
+        bm.start_multiplex_socket(pairList, process_message)
+        bm.start()
+        time.sleep(restartPeriodSeconds)
+        bm.close()
+    except Exception as error:
+        logger.error("Error restarting Binance Socket Manager: " + type(error).__name__ + " " + str(error.args))
+        metrics.putError()
