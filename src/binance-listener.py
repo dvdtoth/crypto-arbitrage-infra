56a26747daaf7077b765d37720d2ddcff540f6a1
Gabor Barany <gabor@cleenup.com>
2019-03-17 20:22:34 +0000

    debug support

--- src/binance-listener.py
+++ src/binance-listener.py
@@ -36,7 +36,7 @@ def getCredentials():
 
 # configurable parameters
 orderbookDepth = 20
-restartPeriodSeconds = 5
+restartPeriodSeconds = 0.5*3600
 
 credentials = getCredentials()
 client = Client(api_key=credentials['api_key'],
@@ -58,7 +58,7 @@ def process_message(msg):
         p = json.dumps(payload, separators=(',', ':'))
         kafka_producer.send(config['kafka']['topic'], p)
         metrics.put(timestamp)
-        print(p)
+        #print(p)
     except Exception as error:
         logger.error("Error in Binance web socket connection: " + type(error).__name__ + " " + str(error.args))
         metrics.putError()
