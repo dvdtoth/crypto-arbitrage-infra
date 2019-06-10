80befa0dda6103ec658e2eeeff7025a095700a4d
dvdtoth <dvdtoth@users.noreply.github.com>
2019-06-10 14:20:12 +0100

    Only allow aggregated metrics

--- src/CWMetrics.py
+++ src/CWMetrics.py
@@ -15,7 +15,7 @@ class CWMetrics:
         self.exchange = exchange
 
         self.metric_data = []
-        self.last_put_time = 0
+        self.last_append_time = 0
         self.count_samples = 0
         self.cloudwatch = boto3.client('cloudwatch', region_name='eu-west-1')
         #self.cloudwatch = cw
@@ -31,7 +31,8 @@ class CWMetrics:
                     'Unit': 'Count',
                     'Value': 1
                 }]
-        response = self.cloudwatch.put_metric_data(Namespace='TEST/CMC', MetricData=data)
+        #response = self.cloudwatch.put_metric_data(Namespace='TEST/CMC', MetricData=data)
+        return
     
     def putCMCError(self):
 
@@ -41,7 +42,8 @@ class CWMetrics:
                     'Unit': 'Count',
                     'Value': 1
                 }]
-        response = self.cloudwatch.put_metric_data(Namespace='TEST/CMC', MetricData=data)
+        #response = self.cloudwatch.put_metric_data(Namespace='TEST/CMC', MetricData=data)
+        return
 
 
     def putError(self):
@@ -58,13 +60,14 @@ class CWMetrics:
                     'Unit': 'Count',
                     'Value': 1
                 }]
-        response = self.cloudwatch.put_metric_data(Namespace='TEST/ORDERBOOK', MetricData=error_data)
+        #response = self.cloudwatch.put_metric_data(Namespace='TEST/ORDERBOOK', MetricData=error_data)
+        return
 
     def put(self, timestamp):
 
-        # count samples for 1 second
-        if (int(time.time()) - self.count_interval) < self.last_put_time:
-            self.count_samples += 5
+        # count samples for 5 second
+        if (int(time.time()) - self.count_interval) < self.last_append_time:
+            self.count_samples += 1
         else:
             self.metric_data.append({
                     'MetricName': 'ORDERBOOK_INGEST',
@@ -79,21 +82,8 @@ class CWMetrics:
                     'Value': self.count_samples
                 })
             self.count_samples = 0
-            self.last_put_time = int(time.time())
+            self.last_append_time = int(time.time())
         # Send batches of 20
         if len(self.metric_data) == 20:
-#            print("SENDING BATCH")
- #           print(self.metric_data)
             response = self.cloudwatch.put_metric_data(Namespace='TEST/ORDERBOOK', MetricData=self.metric_data)
             self.metric_data = []
-            self.last_put_time = int(time.time())
-
-        # print('SIZE IS:')
-        # print(sys.getsizeof(self.metric_data))
-        # # Put batch when we reached 150 metrics or 30 seconds
-        # if  len(self.metric_data) == 20 or (int(time.time()) - self.count_interval) > self.last_put_time:
-        #     print('READY')
-        #     print(self.metric_data)
-        #     response = self.cloudwatch.put_metric_data(Namespace='TEST/ORDERBOOK', MetricData=self.metric_data)
-        #     self.last_put_time = int(time.time())
-        #     self.metric_data = []
